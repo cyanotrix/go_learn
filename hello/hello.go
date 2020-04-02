71749414c41aa598e1b2487ba4280bc0764d46e9
cyanotrix <nandan.kulkarni1990@gmail.com>
2020-04-02 16:03:35 +0530

    added test case for reverse function

--- hello/hello.go
+++ hello/hello.go
@@ -4,9 +4,12 @@ import (
 	"example.com/user/hello/morestrings"
 
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(morestrings.ReverseRunes("Hello Go!"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
