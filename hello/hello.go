633d73a202677987f136f0e843e4ddadc4f604d7
cyanotrix <nandan.kulkarni1990@gmail.com>
2020-04-02 18:15:37 +0530

    "mutiple return function swap"

--- hello/hello.go
+++ hello/hello.go
@@ -13,6 +13,10 @@ func add(a, b int) int {
 	return a + b
 }
 
+func swap(a, b string) (string, string) {
+	return b, a
+}
+
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(morestrings.ReverseRunes("Hello Go!"))
@@ -21,4 +25,8 @@ func main() {
 	fmt.Printf("Sum of 45 and 29 is %d", Sum(45, 29))
 	fmt.Printf("\n")
 	fmt.Printf("Add of 786 and 233 is %d", add(786, 233))
+	fmt.Printf("\n")
+
+	a, b := swap("hello", "world")
+	fmt.Printf("Swapping 2 string hello and world %q, %q", a, b)
 }
