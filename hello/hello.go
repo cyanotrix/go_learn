c0420ad91850b99224360f447743d70dad75532a
cyanotrix <nandan.kulkarni1990@gmail.com>
2020-04-02 18:23:26 +0530

    added split function

--- hello/hello.go
+++ hello/hello.go
@@ -9,24 +9,40 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+// Same type parameters can be clubbed.
 func add(a, b int) int {
 	return a + b
 }
 
+// Function can have multiple returns.
 func swap(a, b string) (string, string) {
 	return b, a
 }
 
+// Naked return values example.
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
+	// Hello world.
 	fmt.Println("hello world")
+
+	// Packages and reverserunes function.
 	fmt.Println(morestrings.ReverseRunes("Hello Go!"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 
-	fmt.Printf("Sum of 45 and 29 is %d", Sum(45, 29))
-	fmt.Printf("\n")
-	fmt.Printf("Add of 786 and 233 is %d", add(786, 233))
-	fmt.Printf("\n")
+	// Sum and add.
+	fmt.Printf("Sum of 45 and 29 is %d\n", Sum(45, 29))
+	fmt.Printf("Add of 786 and 233 is %d\n", add(786, 233))
 
+	// Swap.
 	a, b := swap("hello", "world")
-	fmt.Printf("Swapping 2 string hello and world %q, %q", a, b)
+	fmt.Printf("Swapping 2 string hello and world %q, %q\n", a, b)
+
+	// Split.
+	x, y := split(43)
+	fmt.Printf("Split of integer 43 are %d, %d\n", x, y)
 }
